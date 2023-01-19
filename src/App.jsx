1814baa46d31b306c9dba1c9cbbe02f4edd7da50
LuckyFox <noah.grebet@orange.fr>
2023-01-19 11:20:49 +0100

    :sparkles: First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-center my-5">React JSON form renderer</h1>
+    </div>
+  )
+}
+
+export default App
